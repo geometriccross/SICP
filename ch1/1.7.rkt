5f6a45c1f45bb5dc415cdc0fdfa9c94a07e558f4
geometriccross <geometriccross@desktop.>
2025-03-12 02:13:42 +0900

    関数を追加

--- ch1/1.7.rkt
+++ ch1/1.7.rkt
@@ -3,3 +3,8 @@
 
 (#%require "../util.rkt")
 
+; Newton法を一回行う
+(define (newton pre x)
+  (let* ((quo (/ x pre)))
+    (displayln (string-append "quo:" (number->string quo)))
+    (/ (+ quo pre) 2)))
