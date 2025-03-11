2c43d29703b4581b41279e917298f98f3225ba56
geometriccross <geometriccross@desktop.>
2025-03-11 22:27:10 +0900

    add code

--- ch1/1.5.rkt
+++ ch1/1.5.rkt
@@ -1,5 +1,13 @@
 #!/usr/bin/env racket
-#lang scheme
+#lang sicp
 
-作用的順序では無限ループ
-正規的順序では0
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+(test 0 (p))
+; 作用的順序では無限ループ
+; 正規的順序では0
