f62219441ff762fec5c26d5037395572ad8fe11c
geometriccross <geometriccross@desktop.>
2025-03-12 00:37:41 +0900

    add new function and pattern

--- run.rkt
+++ run.rkt
@@ -5,14 +5,37 @@
   (display x)
   (newline))
 
+(define (lndisplay x)
+  (newline)
+  (display x))
+
+(define (lndisplayln x)
+  (newline)
+  (display x)
+  (newline))
+
 ; 1.6.rkt
 (#%require "./ch1/1.6.rkt")
 
-(display (sqrt 9))
-(display (sqrt (+ 100 37)))
+(displayln (sqrt 2))
+;; (display (sqrt (+ 100 37)))
 (newline)
 ; (display (sqrt-newif 3))
 
-(displayln
- (string-append "good-enough in 1.4142 vs 2: "
-                (if (good-enough? 1.4142 2) "#t" "#f")))
+(lndisplayln "Correct Pattern")
+(displayln "guess: 1.4142156862745097, x: 2, 許容値: 0.001")
+(displayln (string-append
+            "expect: #t, actual: "
+            (if (good-enough? 1.4142156862745097 2) "#t" "#f")))
+
+(lndisplayln "SMALL NUMBER")
+(displayln "guess: 0.01 x: 0.0001, 許容値: 0.001")
+(displayln (string-append
+            "expect: #f, actual: "
+            (if (good-enough? 0.1 0.01) "#t" "#f")))
+
+(lndisplayln "BIG NUMBER")
+(displayln "guess: 501, x: 2500, 許容値: 0.001")
+(displayln (string-append
+            "expect: #t, actual: "
+            (if (good-enough? 500 2500) "#t" "#f")))
