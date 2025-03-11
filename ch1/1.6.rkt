050e075e17fad07a6a6d0081cc54e7ac7cf13f54
geometriccross <geometriccross@desktop.>
2025-03-11 22:17:45 +0900

    モジュールにした

--- ch1/1.6.rkt
+++ ch1/1.6.rkt
@@ -1,44 +1,47 @@
 #!/usr/bin/env racket
 #lang sicp
 
+(#%provide square)
 (define (square x) (* x x))
 
+(#%provide average)
 (define (average x y)
   (/ (+ x y) 2))
 
+(#%provide improve)
 (define (improve guess x)
   (average guess (/ x guess)))
 
+(#%provide good-enough?)
 (define (good-enough? guess x)
   (< (abs (- (square guess) x)) 0.001))
 
+(#%provide sqrt-iter)
 (define (sqrt-iter guess x)
   (if (good-enough? guess x)
       guess
       (sqrt-iter (improve guess x)
                  x)))
 
+(#%provide sqrt)
 (define (sqrt x)
   (sqrt-iter 1.0 x))
 
-(sqrt 9)
-
-(sqrt (+ 100 37))
-
 ;; 問1.6
 
+(#%provide new-if)
 (define (new-if predicate then-clause else-clause)
-    (cond (predicate then-clause)
-          (else else-clause)))
+  (cond (predicate then-clause)
+        (else else-clause)))
 
+(#%provide sqrt-iter-newif)
 (define (sqrt-iter-newif guess x)
   (new-if (good-enough? guess x)
           guess
           (sqrt-iter (improve guess x) x)))
 
+(#%provide sqrt-newif)
 (define (sqrt-newif x)
   (sqrt-iter-newif 1.0 x))
-
-(sqrt-newif 2)
 
 ;; 無限ループに陥る
