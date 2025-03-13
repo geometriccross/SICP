e9a8eef1cfe62f9de3744c65f4f5e373ceeaae95
geometriccross <geometriccross@desktop.>
2025-03-13 23:21:12 +0900

    片方の関数トレースを完了

--- ch1/1.9.rkt
+++ ch1/1.9.rkt
@@ -1,3 +1,16 @@
 #!/usr/bin/env racket
 #lang sicp
 
+;; (define (+ a b)
+;;   (if (= a 0)
+;;       b
+;;       (inc (+ (dec a) b))))
+
+;; (+ 4 5)
+;; (inc (+ (dec 4) 5))
+;; (inc (inc (+ (dec 3) 5)))
+;; (inc (inc (inc (+ (dec 2) 5))))
+;; (inc (inc (inc (inc (+ (dec 1) 5)))))
+;; (inc (inc (inc (inc 5)))))
+;; 9
+
